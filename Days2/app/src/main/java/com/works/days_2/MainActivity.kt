eb460174bae16a66de864f9ca8a70d082cef9758
Hakan ÖZER <hakanozer02@gmail.com>
2023-12-19 12:30:32 +0300

    2-Days

--- Days2/app/src/main/java/com/works/days_2/MainActivity.kt
+++ Days2/app/src/main/java/com/works/days_2/MainActivity.kt
@@ -3,6 +3,8 @@ package com.works.days_2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.works.days_2.utils.EApp
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
@@ -49,7 +51,84 @@ class MainActivity : AppCompatActivity() {
             Log.d("item", item)
         }
 
+        var start = System.currentTimeMillis()
+        val arr = mutableListOf<String>()
+        arr.add("Ali")
+        arr.add("Zehra")
+        arr.add("Zehra")
+        arr.add("Ayşe")
+        arr.add("Selin")
+        arr.add("Selin")
+        arr.add("Selin")
+        arr.add("Ahmet")
+        arr.add("Kemal")
+        arr.add("Kemal")
+        Log.d("arr", arr.toString())
+        var end = System.currentTimeMillis()
+        var bettween = end - start
+        Log.d("mutableListOf", bettween.toString())
 
+        // delete item
+        //arr.removeAt(0)
+        //arr.remove("ayşe")
+
+        for ( i in 0..arr.count() - 1) {
+            Log.d("i", arr.get(i))
+        }
+
+
+
+        // Set
+        start = System.currentTimeMillis()
+        val set = mutableSetOf<String>()
+        set.add("Ali")
+        set.add("Zehra")
+        set.add("Zehra")
+        set.add("Ayşe")
+        set.add("Selin")
+        set.add("Selin")
+        set.add("Selin")
+        set.add("Ahmet")
+        set.add("Kemal")
+        set.add("Kemal")
+        Log.d("set", set.toString())
+        end = System.currentTimeMillis()
+        bettween = end - start
+        Log.d("mutableSetOf", bettween.toString())
+
+        // Map
+        val map = mutableMapOf<String, Any>()
+        map.put("city", "İstanbul")
+        map.put("name", "Kemal")
+        map.put("email", "zehra@mail.com")
+        map.put("name", "Kenan")
+        map.put("name", "Zehra")
+        map.put("surname", "Bilmem")
+        map.put("status", true)
+        map.put("age", 35)
+        Log.d("name", map.get("name").toString())
+        Log.d("Map", map.toString())
+
+        val keys = map.keys
+        for (key in keys) {
+            val data = map.get(key)
+            Log.d("key", key)
+            Log.d("val", data.toString())
+            if (data is Int) {
+                val numConvert = data.toInt()
+                Log.d("numConvert", "${numConvert + 10}")
+            }
+        }
+
+        for ( (k, v) in map ) {
+            Log.d(k, v.toString())
+        }
+
+        val map1 = mutableMapOf<EApp, Any>()
+        map1.put(EApp.NAME, "Sultan")
+        map1.put(EApp.SURNAME, "Bil")
+        map1.put(EApp.AGE, 30)
+        Log.d("EApp", map1.toString())
 
 
     }
---
+++ Days2/app/src/main/java/com/works/days_2/utils/EApp.kt
@@ -0,0 +1,7 @@
+package com.works.days_2.utils
+
+enum class EApp {
+
+    NAME, SURNAME, AGE
+
+}
