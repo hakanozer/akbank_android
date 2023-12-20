56c450e984fab23503c9a1103f60b745b382e679
Hakan ÖZER <hakanozer02@gmail.com>
2023-12-20 15:53:38 +0300

    Project

--- Project/app/src/main/java/com/works/project/MainActivity.kt
+++ Project/app/src/main/java/com/works/project/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.works.project
 
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextWatcher
@@ -9,7 +11,10 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
+import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
 import com.works.project.configs.ApiClient
 import com.works.project.models.Customer
 import com.works.project.models.JWTUser
@@ -27,6 +32,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var l_btnSingUp: Button
     lateinit var dummyService: DummyService
 
+    lateinit var sharedPreferences: SharedPreferences
+    lateinit var editor: Editor
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +46,8 @@ class MainActivity : AppCompatActivity() {
         l_btnSingUp = findViewById(R.id.l_btnSingUp)
 
         dummyService = ApiClient().getClient().create(DummyService::class.java)
+        sharedPreferences = getSharedPreferences("user", MODE_PRIVATE)
+        editor = sharedPreferences.edit()
 
         // Username Text Change
         l_txtUsername.addTextChangedListener {
@@ -55,24 +65,36 @@ class MainActivity : AppCompatActivity() {
     val sendClick = View.OnClickListener {
         val username = l_txtUsername.text.toString()
         val password = l_txtPassword.text.toString()
-        val jwtUser = JWTUser(username, password)
-        dummyService.login(jwtUser).enqueue( object : Callback<User> {
-            override fun onResponse(call: Call<User>, response: Response<User>) {
-                val status = response.code()
-                if (status == 200) {
-                    val data = response.body()
-                    data?.let {
-                        Log.d("json", "Login Success ${it}")
+        if ( username == "" ) {
+            Toast.makeText(this, "Username Empty!", Toast.LENGTH_SHORT).show()
+            l_txtUsername.requestFocus()
+        }else if ( password == "" ) {
+            Toast.makeText(this, "Password Empty!", Toast.LENGTH_SHORT).show()
+            l_txtPassword.requestFocus()
+        }else {
+            val jwtUser = JWTUser(username, password)
+            dummyService.login(jwtUser).enqueue( object : Callback<User> {
+                override fun onResponse(call: Call<User>, response: Response<User>) {
+                    val status = response.code()
+                    if (status == 200) {
+                        val data = response.body()
+                        data?.let {
+                            val gson = Gson()
+                            val data:String = gson.toJson(it)
+                            Log.d("json", data)
+                            editor.putString("user", data)
+                            editor.commit()
+                        }
+                    }else {
+                        Snackbar.make(l_btnSend, "Username or Password Fail!", Snackbar.LENGTH_LONG).show()
                     }
-                }else {
-                    Log.d("json", "Username or Password Fail!")
                 }
-            }
 
-            override fun onFailure(call: Call<User>, t: Throwable) {
-                Log.e("json", "onFailure: ", t )
-            }
-        })
+                override fun onFailure(call: Call<User>, t: Throwable) {
+                    Snackbar.make(l_btnSend, t.toString(), Snackbar.LENGTH_LONG).show()
+                }
+            })
+        }
 
     }
 
